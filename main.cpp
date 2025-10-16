0e44247f959afbee28ba868a4184b9a221f46632
nstamonica <nathanstamonica@gmail.com>
2025-10-15 21:18:19 -0400

    Completed Exercise 1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// Exercise 1
+
+// TODO: Declare an integer variable 'x' and initialize it to 42
+int x = 42;
+
+// TODO: Declare a pointer 'ptr' that points to x
+int* ptr = &x;
+
+// TODO: Print the value of x using both x and *ptr
+std::cout << x << std::endl;
+std::cout << *ptr << std::endl;
+
+// TODO: Print the address of x using both &x and ptr
+std::cout << &x << std::endl;
+std::cout << ptr << std::endl;
+
+// TODO: Change the value of x to 100 using the pointer
+int* ptr2 = &x;
+*ptr2 = 100;
+
